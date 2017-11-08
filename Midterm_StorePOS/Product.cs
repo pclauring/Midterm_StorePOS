55c9dfdb8d63c7c6c04b1fabbbf9809f95c1bedd
Pierce C Lauring <pclauring@gmail.com>
2017-11-08 13:06:06 -0500

    Added README.MD

---
+++ Midterm_StorePOS/Product.cs
@@ -0,0 +1,50 @@
+﻿namespace Midterm_StorePOS
+{
+    class Product
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string category;
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
--- Midterm_StorePOS/Program.cs
+++ Midterm_StorePOS/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using System.IO;
+using System;
+
 
 namespace Midterm_StorePOS
 {
+
     class Program
     {
+        const string FILENAME = "inventory.txt";
         static void Main(string[] args)
         {
+            StreamReader menuMaker = new StreamReader(FILENAME);
+            ArrayList menu = new ArrayList();
+            while (true)
+            {
+                Product menuItem = new Product();
+                string menuLine = menuMaker.ReadLine();
+                if (menuLine == null || menuLine == "")
+                {
+                    break;
+                }
+
+                string[] itemParts = menuLine.Split('\t');
+                itemParts[0] = menuItem.Name;
+                itemParts[1] = menuItem.Category;
+                itemParts[2] = menuItem.Description;
+                // needs better validation
+                double.TryParse(itemParts[3], out double price);
+                price = menuItem.Price;
+                // needs better validation
+                int.TryParse(itemParts[4], out int quantity);
+                quantity = menuItem.Quantity;
+                menu.Add(menuItem);
+
+            }
+            menuMaker.Close();
+            foreach (Product item in menu)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
