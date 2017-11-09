7a8765e297860bceda20e7ce8b140ebaa7d1d7b1
Pierce C Lauring <pclauring@gmail.com>
2017-11-09 09:46:13 -0500

    Pulled itemized method to cart & removed redundant checkout

--- Midterm_StorePOS/Checkout.cs
+++ Midterm_StorePOS/Checkout.cs
@@ -123,7 +123,7 @@ namespace Midterm_StorePOS
             return sum % 10 == 0;
         }
 
-        public static bool VerifyCardExpire(string cardExpire)
+        /*public static bool VerifyCardExpire(string cardExpire)
         {
             cardExpire = cardExpire.Replace("/", "");
             int[] numbers = new int[cardExpire.Length];
@@ -131,7 +131,7 @@ namespace Midterm_StorePOS
             {
                 numbers[i] = Int32.Parse(cardExpire.Substring(i, 1));
             }
-        }
+        } */
 
         public static bool VerifyCardCVV(int cardCVV)
         {
